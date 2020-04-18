4ce38393f317b350ed70bb2b0076a8cbb38effb2
Sebastian Wueste <swues002@wch136-21.cs.ucr.edu>
2020-04-18 14:46:40 -0700

    Submitting Part One

---
+++ Lab4_SM/source/main.c
@@ -0,0 +1,57 @@
+/*	Author: swues002
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum States {Start, Zero, One} state;
+unsigned char tempA = 0x00;
+
+void tick(){
+	tempA = PINA;
+	switch(state) { //transitions
+	    case Start:
+	        state = Zero;
+		break;
+	    case Zero:
+		state = ((tempA & 0x01) == 0x01) ? One : Zero;
+		break;
+	    case One:
+		state = ((tempA & 0x01) == 0x01) ? Zero : One;
+		break;
+	    default:
+		state = Start;
+		break;
+	} //transitions
+	switch(state) { //state actions
+	    case Start:
+		break;
+	    case Zero:
+		PORTB = 0x01;
+		break;
+	    case One:
+		PORTB = 0x02;
+		break;
+	    default: break;
+	} //state actions
+		
+}
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+ 	DDRB = 0xff; PORTB = 0x00;
+	state = Start;
+    /* Insert your solution below */
+    while (1) {
+	tick();
+    }
+    return 1;
+}
