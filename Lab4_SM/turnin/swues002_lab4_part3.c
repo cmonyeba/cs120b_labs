f4003a99da1ff3efb5d2bc4152c18f16f5d054a4
Sebastian Wueste <swues002@wch136-21.cs.ucr.edu>
2020-04-18 18:11:32 -0700

    turnin files

---
+++ Lab4_SM/turnin/swues002_lab4_part3.c
@@ -0,0 +1,91 @@
+/*	Author: swues002
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum States {Start, Locked, WaitFirst, WaitFall, WaitSecond, Unlocked} state;
+unsigned char input = 0x00;
+unsigned char temp = 0x00;
+
+void tick(){
+	input = PINA;
+	
+	switch(state) { //transitions
+	    case Start:
+	        state = Locked;
+		break;
+	    case Locked:
+		state = WaitFirst;
+		break;
+	    case WaitFirst:
+		if(input == 0x01 || input == 0x02 || input == 0x04){
+		    if(input == 0x04){
+			temp = 1;
+		    }
+		    state = WaitFall;
+		}
+		else{
+		   state = WaitFirst;
+		}
+		break;
+	    case WaitFall:
+		if(input == 0x00){
+		    state = WaitSecond;
+		}
+		else{
+		    state = WaitFall;
+		}
+		break;
+	    case WaitSecond:
+		if(input == 0x01 || input == 0x02 || input == 0x04){
+		    if(input == 0x02 && temp == 1){
+			    state = Unlocked;
+		    }
+		    else{
+		        state = Locked;
+		    }
+		}
+		else{
+		    state = WaitSecond;
+		}
+		break;
+	    case Unlocked:
+		if(input == 0x40){
+	            state = Locked;
+		}
+		break;
+	    default:
+		state = Start;
+		break;
+	} //transitions
+	switch(state) { //state actions
+	    case Locked:
+		PORTB = 0x00;
+		break;
+	    case Unlocked:
+		PORTB = 0x01;
+		break;
+	    default: break;
+	} //state actions
+		
+}
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+ 	DDRB = 0xFF; PORTB = 0x00;
+	state = Start;
+    /* Insert your solution below */
+    while (1) {
+	tick();
+    }
+    return 1;
+}
