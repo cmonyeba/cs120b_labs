8dbd4833065710f4fe34dcc2dd270b674cb037d2
cmonyeba <cmonyeba@github.com>
2020-04-22 01:28:11 -0700

    Submitting parts 1 + 2

---
+++ Lab5/turnin/swues002_lab5_part1.c
@@ -0,0 +1,51 @@
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+    /* Insert your solution below */
+    unsigned char button = 0x00;
+    unsigned char temp = 0x00;
+    unsigned char led = 0x00;
+    
+    while (1) {
+        temp = PINA;
+	button = ~temp; //buttons
+        if(button==0x00){
+		led = 0x40;
+	}
+	if(button==0x01 || button==0x02){
+		led = 0X60;
+	}
+	if(button==0x03 || button==0x04){
+		led = 0x70;
+	}
+	if(button==0x05 || button==0x06){
+		led = 0x38;
+	}
+	if(button>=0x07 && button<=0x09){
+		led = 0x3C;
+	}
+	if(button>=0x0A && button<=0x0C){
+		led = 0x3E;
+	}
+	if(button>=0x0D && button<=0x0F){
+		led = 0x3F;
+	}
+	PORTC = led;
+    }
+    return 1;
+}
---
+++ Lab5/turnin/swues002_lab5_part2.c
@@ -0,0 +1,86 @@
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum States {Start, Rest, Fall, Inc, Dec, Clear} state;
+unsigned char button = 0x00;
+unsigned char temp = 0x00;
+unsigned char val = 0x00;
+void tick(){
+	temp = PINA;
+	button = ~PINA&0x03;
+
+	switch(state){//transitions
+		case Start: state=Rest; break;
+                case Rest:
+			    if(button==0x01){
+				    state = Inc;
+			    }
+			    else if(button==0x02){
+				    state = Dec;
+			    }
+			    else if(button==0x03){
+				    state = Clear;
+			    }
+			    else{
+				    state = Rest;
+			    }
+			    break;
+		case Fall://waits for button to be released
+			    if(button==0x00){
+				    state = Rest;
+			    }
+			    else if(button==0x03){
+				    state = Clear;
+			    }
+			    else{
+				    state = Fall;
+			    }
+			    break;
+		case Inc: state=Fall; break;
+	        case Dec: state=Fall; break;
+		case Clear: state=Fall; break;
+		default: state=Start; break;
+	}//transitions
+	
+	switch(state){//state actions
+		case Inc:
+			if(val<0x09){
+				val++;
+			}
+			break;
+		case Dec:
+			if(val>0){
+				val--;
+			}
+			break;
+		case Clear:
+			val = val * 0;
+			break;
+		case Fall: 
+			PORTC = val;
+			break;
+		default:break;
+	}//state actions
+}
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+    /* Insert your solution below */
+    state = Start;
+    while (1) {
+     	tick();
+    }
+    return 1;
+}
